7b9434fcf50c8b03bb68111458cd4f5727b92bdf
yogeshbansal11 <bansalindia850@gmail.com>
2026-04-25 23:43:48 +0530

    fix: bug fix

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.get('/health', (req, res) => {
 // Serve built frontend
 const distPath = path.join(__dirname, '../dist');
 app.use(express.static(distPath));
-app.get('*', (req, res) => {
+app.get('/{*path}', (req, res) => {
   res.sendFile(path.join(distPath, 'index.html'));
 });
 
